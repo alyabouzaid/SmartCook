b8914ed7f678780459e563bb8d7a256bba4a8597
alyabouz <z5c9@ugrad.cs.ubc.ca>
2020-06-05 20:43:23 -0700

    assignment1

---
+++ frontend/src/components/ingredientInventory/IngredientInventory.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addingIngredient } from '../../actions/ingredientInventoryActions';
+import { clearIngredients } from '../../actions/ingredientInventoryActions';
+
+class IngredientInventory extends React.Component {
+
+
+
+
+    addItem() {
+
+
+        let inventory =  document.getElementById("inventory").value
+        let amount =  document.getElementById("amount").value
+
+        this.props.addingIngredient({
+            "key": this.props.ingredientInventory.length,
+            "description": inventory,
+            "amount":amount
+        })
+
+    }
+
+    detailsMessage(i) {
+        
+        // {console.log(i)}
+
+        var workshops_content = document.getElementById("itemclickedbox");
+        workshops_content.value = i
+        console.log(workshops_content)
+        
+    }
+
+
+
+	render() {
+		return (				
+        
+
+        <div id="ingredientInventory" className="content" text-align="center" >
+                        
+
+
+            <h1>Ingredient Inventory </h1>
+
+            <p>	
+                 <b>Ingredient:</b>
+				<input type="text" id="inventory" name="fname" size="100" />
+			</p>
+
+
+
+			<p>	
+                 <b>Amount:</b>
+				<input type="text" id="amount" name="fname" size="100" />
+			</p>
+
+
+
+            <p>
+            <button onClick={() => { this.addItem()}}>add item</button>
+            </p>
+
+
+            <ul>
+
+
+                    {(this.props.ingredientInventory).map(item => ( 
+                    <div>
+
+                    {/* <button onClick={() => { this.detailsMessage( "   " +item.description+  "  item number:  "+ item.key)}}>Show date</button> */}
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        {item.description} 
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        {item.amount  }
+                    </div>
+                    ))}
+            </ul>
+
+            {/* <input  id="itemclickedbox" value="No item clicked yet" size="100"/> */}
+
+			<p>	
+				<button onClick={() =>  this.props.clearIngredients([]) }>clear inventory</button>
+			</p>
+
+
+
+        </div>);
+	}
+}
+
+//state has entire state of app!!
+const mapStateToProps = (state) => { //name is by convention
+    return { ingredientInventory: state.ingredientInventory}; //now it will appear as props
+}
+    
+
+export default connect(mapStateToProps, { addingIngredient,clearIngredients })(IngredientInventory);
