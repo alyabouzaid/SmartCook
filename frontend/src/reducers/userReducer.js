34be389ff03edc49c6c80618d95ba55bb7a7903d
leesamantha <leesamantha@live.com>
2020-07-12 09:12:37 -0700

    refactored server and user reducer, removed loadUserData and title heading from components

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-const initialState = { isAuthenticated: false, name: "", email: "" };
+const initialState = {"isLoggedIn": false,"firstName": "","email": ""};
 
 export default function userReducer(state = initialState, action) {
   if (action.type === "USER_LOAD") {
