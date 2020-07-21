b84a04bbff707e39c96b9f0fff80af55a1961719
hkhx417 <hkhx417@students.cs.ubc.ca>
2020-07-21 11:26:15 -0700

    Refactored project structure for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import LandingPage from "./components/landingPage/landingPage";
 import IngredientInventory from "./components/ingredientInventory/IngredientInventory";
-import Header from "./components/landingPage/Header";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
