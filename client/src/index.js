430b7af76b81b3ba8dc7f058b84e004e9b454538
leesamantha <leesamantha@live.com>
2020-08-06 00:06:22 -0700

    make colour mode persist with refresh using redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,31 @@ import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+// Source from: https://blog.reactnativecoach.com/the-definitive-guide-to-redux-persist-84738167975
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: [
+        "colorStore"
+    ]
+
+};
+
+const pReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(pReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
 
 ReactDOM.render(
     <BrowserRouter>
-        <Provider store={createStore(reducers, applyMiddleware(thunk))}>
-            <App />
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </BrowserRouter>,
     document.getElementById("root")
